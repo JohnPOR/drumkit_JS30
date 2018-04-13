eec27a0b4b773ad2403ae288c1667587b46cc32d
ss10j <john.paul.oreilly01@gmail.com>
2018-04-13 19:39:51 +0100

    sounds, html update, css tweak

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //function to play sound (e)
 function playsound (e) {
     const audio = document.querySelector(`audio[data-key = "${e.keycode}"]`);
-    const key = document.querySelector(`.key[data-key = "${e.keycode}"`)            
+    const key = document.querySelector(`.key[data-key = "${e.keycode}"`)
     if(!audio) return //if pressed key not assigned to audio, return
     audio.currentTime = 0; //rewinds audio to start, otherwise overlap occurs
     audio.play();
@@ -14,11 +14,11 @@ function removeTransition(e) {
     this.classlist.remove('playing'); //removes class of 'playing' once played
 }
 
-//listens for all keys on document 
-const keys = document.querySelectorAll('.key') 
+//listens for all keys on document
+const keys = document.querySelectorAll('.key')
 
 //ends key transition by using function removeTransition. Transition includes CSS
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 //listen for keydown event (e)
-window.addEventListener('keydown', playsound);  
+window.addEventListener('keydown', playsound);
